2bb542acabfb3948872b9145eb2f65e22c4e88d7
SzymonLeszkiewicz <szymon.leszkiewicz10@gmail.com>
2024-06-21 14:48:03 +0200

    fix: datetime import

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from gc_calendar import (authenticate_google_calendar,
                          get_events, list_calendars)
 import streamlit as st
-import datetime
+from datetime import datetime
 
 # config st page
 st.set_page_config(
@@ -14,29 +14,39 @@ st.set_page_config(
 col1, col2 = st.columns(2)
 
 with col1:
-    st.header("GC Lessons Counter")
+    st.write("# GC Lessons Counter 📅")
     st.write("This app will count the number of lessons in a given month.")
     service = authenticate_google_calendar()
     calendar_lists = list_calendars(service)
-    st.write("## Select calendar:")
+    st.write("### Select calendar:")
     calendar_id = st.selectbox("Choose the calendar", calendar_lists)
-    st.write("## Select month:")
+    st.write("### Select month:")
     month = st.selectbox("Choose the month", list(
         range(1, 13)), index=datetime.now().month - 1)
-    st.write("## Select year:")
+    st.write("### Select year:")
     year = st.selectbox("Choose the year", list(
         range(2021, 2025)), index=datetime.now().year - 2021)
 
-    events = get_events(service, calendar_id, month, year)
-    if not events:
-        st.write("No lessons found in this month.")
-    else:
-        st.write("## Lessons:")
-        for event in events:
-            st.write(f"- {event['summary']}")
-        st.write(f"Total number of lessons in {month}/{year}: {len(events)}")
+    try:
+        events = get_events(service, calendar_id, month, year)
+        if not events:
+            st.write("No lessons found in this month.")
+        else:
+            st.write("## Lessons:")
+            for event in events:
+                st.write(f"- {event['summary']}")
+            st.write(
+                f"Total number of lessons in {month}/{year}:"
+                f" {len(events)}")
+    except NameError:
+        st.write("Nie masz pełnych uprawnień do tego kalendarza!")
 
 with col2:
-    st.write(f"## Stats of {month}/{year} 📊")
-    st.write(f"Number of lessons in {month}/{year}:")
-    st.write(len(events))
+    try:
+        st.write(f"## Stats of {month}/{year} 📊")
+        les_counter = len(events)
+        st.write(f"Number of lessons in {month}/{year}:")
+        st.write(les_counter)
+
+    except NameError:
+        st.write("Nie masz pełnych uprawnień do tego kalendarza!")
